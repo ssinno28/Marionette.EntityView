03a72730a617449cbbc28a51a9cf026604af5121
ssinno28 <sammi.sinno@gmail.com>
2017-02-06 22:31:03 -0500

    updating tests to have a body in the dom
    adding test to make sure getAll is called when layout vie wis already in the region
    updating MockEntithCollection with query method that returns promise

--- tests/entityServiceTests.js
+++ tests/entityServiceTests.js
@@ -1,15 +1,24 @@
 describe('Entity Service', function () {
     var App = new Marionette.Application(),
-        MockEntityCollection = Backbone.EntityCollection.extend({});
+        MockEntityCollection = Backbone.EntityCollection.extend({
+            query: function (track, data, force) {
+                return $.Deferred(function(defer){
+                    defer.resolve(new Backbone.Collection());
+                });
+            }
+        }),
+        html = '<html><body><div id="test-region"></div></body></html>';
+
+    $(document.body ).append(html);
 
     var options = {
         collection: new MockEntityCollection(),
         model: Backbone.Model,
         formView: Marionette.EntityFormView,
         listView: Marionette.EntityListView,
-        title: 'Extension Types',
-        route: 'extension-type',
-        region: new Backbone.Marionette.Region({el: document.createElement('div')})
+        title: 'Testing',
+        route: 'test',
+        region: new Backbone.Marionette.Region({el: '#test-region'})
     };
 
     var entityService = new Marionette.EntityService(options);
@@ -22,5 +31,11 @@ describe('Entity Service', function () {
         expect(entityService.getChannel()).toEqual(Backbone.Radio.channel(options.route));
     });
 
-    it('calls ')
+    it('calls getAll', function () {
+        entityService.region.show(entityService.entityLayoutView(new Backbone.Collection()));
+        spyOn(entityService, 'getAll');
+
+        entityService.getType(1);
+        expect(entityService.getAll).toHaveBeenCalled();
+    });
 });
