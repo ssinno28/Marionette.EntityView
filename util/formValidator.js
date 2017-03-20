5440dbdd539629a2879793a374056988e3be8c0a
ssinno28 <sammi.sinno@gmail.com>
2017-03-20 19:47:18 -0400

    removing ids from formValidator
    udpating template copyright

--- util/formValidator.js
+++ util/formValidator.js
@@ -19,7 +19,6 @@ var FormValidator;
          },*/
 
         min: {
-            id: '272b4a6f-055f-44af-bd2e-dc8d3f281537',
             evaluate: function (val, options) {
                 if (val.length < options[0]) return false;
                 return true;
@@ -28,7 +27,6 @@ var FormValidator;
 
 
         max: {
-            id: 'c1bfaee0-7179-4764-a92a-42e1afb78b82',
             evaluate: function (val, options) {
                 if (val.length > options[0]) return false;
                 return true;
@@ -36,35 +34,30 @@ var FormValidator;
         },
 
         numeric: {
-            id: '85aee6f9-c7af-4658-b983-4be319e960c4',
             evaluate: function (val) {
                 return !_.isNaN(val);
             }
         },
 
         alpha: {
-            id: 'a355ed4d-6a00-44c9-8052-9361fb285ae5',
             evaluate: function (val) {
                 return FormValidator.regex.alpha.test(val);
             }
         },
 
         alphanum: {
-            id: '63f1cf8b-daed-48f2-a719-a2229de2fd94',
             evaluate: function (val) {
                 return FormValidator.regex.alphanum.test(val);
             }
         },
 
         email: {
-            id: '813dd3a8-c394-496b-a81c-93b97b4bbf87',
             evaluate: function (val) {
                 return val === '' || FormValidator.regex.email.test(val);
             }
         },
 
         required: {
-            id: 'c63eae79-f7b6-4747-88c0-2baa6bffe3e4',
             evaluate: function (val) {
                 if (val === false || _.isNull(val) || _.isUndefined(val) || (_.isString(val) && val.length === 0)) return false;
                 return true;
@@ -72,7 +65,6 @@ var FormValidator;
         },
 
         boolean: {
-            id: 'f23a85d8-389f-44db-bc1d-a9162c4a5580',
             evaluate: function (val) {
                 return _.isBoolean(val);
             }
