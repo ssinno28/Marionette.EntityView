a4729f0fbea52894e3bf404c457c313086c81a98
ssinno28 <sammi.sinno@gmail.com>
2017-04-25 13:14:03 -0400

    Update SortableCollectionBehavior.js

--- behaviors/SortableCollectionBehavior.js
+++ behaviors/SortableCollectionBehavior.js
@@ -2,6 +2,46 @@
 var SortableCollectionBehavior;
 (function ($, _, Backbone, Marionette) {
     SortableCollectionBehavior = Marionette.Behavior.extend({
+        onRender: function(){
+            this.view.$el.addClass('sortable-view');
+            this.setComparator();
+        },
+        setPlacement: function (draggedModel, overModel) {
+            var draggedModelPlacement = draggedModel.get('placement'),
+                overModelPlacement = overModel.get('placement');
 
+            var placement;
+            if (draggedModelPlacement < overModelPlacement) {
+                this.collection.each(function (item) {
+                    var placement = item.get('placement');
+                    if (placement <= overModelPlacement) {
+                        var newPlacement = placement - 1;
+                        item.set({placement: newPlacement});
+                    }
+                });
+
+                draggedModel.set({placement: overModelPlacement});
+            } else if (draggedModelPlacement > overModelPlacement) {
+                this.collection.each(function (item) {
+                    var placement = item.get('placement');
+                    if (placement >= overModelPlacement) {
+                        var newPlacement = placement + 1;
+                        item.set({placement: newPlacement});
+                    }
+                });
+
+                draggedModel.set({placement: overModelPlacement});
+            }
+
+            this.setComparator();
+        },
+        setComparator: function () {
+            this.view.collection.comparator =
+                function (model) {
+                    return model.get('placement');
+                };
+
+            this.view.collection.sort();
+        }
     });
 })(jQuery, _, Backbone, Marionette);
