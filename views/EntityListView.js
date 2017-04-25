c6105bbfb7f244d11b381e49421a6f76210ae363
ssinno28 <sammi.sinno@gmail.com>
2017-04-25 14:05:03 -0400

    adding sortable list behavior and sortable flag to child view

--- views/EntityListView.js
+++ views/EntityListView.js
@@ -1,11 +1,22 @@
 var EntityListView;
-(function ($, _, Backbone, Marionette) {
+(function ($, _, Backbone, Marionette, SortableListBehavior) {
     EntityListView = Marionette.EntityListView = Backbone.Marionette.CollectionView.extend({
         className: 'col-sm-12',
         initialize: function (options) {
             _.extend(this, options);
 
             this._channel = Backbone.Radio.channel(this.route);
+        },
+        behaviors: function(){
+            var behaviors = {};
+          if(this.getOption('sortable'))
+          {
+              behaviors.Sortable = {
+                  behaviorClass: SortableListBehavior
+              };
+          }           
+            
+            return behaviors;
         },
         onDomRefresh: function () {
             this._channel.trigger('view.list.activated');
@@ -24,7 +35,8 @@ var EntityListView;
                 route: route,
                 allowableOperations: allowableOperations,
                 collection: collection,
-                baseClassIds: baseClassIds
+                baseClassIds: baseClassIds,
+                sortable: this.getOption('sortable')
             };
         },
         onAddChild: function (childView) {
@@ -38,4 +50,4 @@ var EntityListView;
         }
     });
 
-})(jQuery, _, Backbone, Marionette);
+})(jQuery, _, Backbone, Marionette, SortableListBehavior);
