06da8ad0397f86441c2a26c64ea57fd3d0afc351
ssinno28 <sammi.sinno@gmail.com>
2017-05-10 12:22:55 -0400

    Update entityFormTests.js

--- tests/entityFormTests.js
+++ tests/entityFormTests.js
@@ -1,9 +1,34 @@
 
 describe('Entity Forms Validation', function () {
+    var formView = Marionette.EntityFormView.extend({
+    onRender: function () {
+        this.field('name')
+            .label('Name')
+            .fieldset('user-settings', 'User Settings')
+            .required('Please enter your full name.')
+            .singleLine();
+
+        this.field('email')
+            .label('Email')
+            .fieldset('user-settings')
+            .required('Please enter an email address.')
+            .email('The email address is not in the correct format!')
+            .singleLine();
+
+        this.field('age')
+            .label('Your Age')
+            .fieldset('user-settings')
+            .required('Your Age is required!!')
+            .numeric('This field should be a number!')
+            .min('There is a minimum age of 23 required!', 23)
+            .singleLine();
+        }
+    });
+    
     var options = {
         collection: new MockEntityCollection(),
         model: Backbone.Model,
-        formView: Marionette.EntityFormView,
+        formView: formView,
         listView: MockListView,
         title: 'Testing',
         route: 'test',
@@ -12,8 +37,11 @@ describe('Entity Forms Validation', function () {
 
     options.collection.add(new Backbone.Model({id: 1, name: 'testing'}));
     var entityService = new Marionette.EntityService(options);
-
-    it('defaults to have routing', function () {
+    entityService.create();
+    
+    it('returns required validation', function () {
+        var errors = region.currentView.validate();
+        
         expect(entityService.routing).toEqual(true);
     });
 });
