2a45d397eab6586bbe27373c03464823f65c35a5
ssinno28 <sammi.sinno@gmail.com>
2017-05-10 12:11:17 -0400

    Update entityFormTests.js

--- tests/entityFormTests.js
+++ tests/entityFormTests.js
@@ -1,1 +1,19 @@
 
+describe('Entity Forms Validation', function () {
+    var options = {
+        collection: new MockEntityCollection(),
+        model: Backbone.Model,
+        formView: Marionette.EntityFormView,
+        listView: MockListView,
+        title: 'Testing',
+        route: 'test',
+        region: region
+    };
+
+    options.collection.add(new Backbone.Model({id: 1, name: 'testing'}));
+    var entityService = new Marionette.EntityService(options);
+
+    it('defaults to have routing', function () {
+        expect(entityService.routing).toEqual(true);
+    });
+});
