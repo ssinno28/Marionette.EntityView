af4a5fc45113151bfb5a40b011e2399a084e41fd
ssinno28 <sammi.sinno@gmail.com>
2017-06-13 20:52:56 -0400

    fixing issue with grecaptcha not rerendering properly

--- views/reusableTypes/captcha/CaptchaView.js
+++ views/reusableTypes/captcha/CaptchaView.js
@@ -1,21 +1,28 @@
 var CaptchaView;
 (function ($, _, Backbone, Marionette, ReusableTypeLayoutView) {
     CaptchaView = ReusableTypeLayoutView.extend({
-        template: _.template('<div id="recaptcha"></div>'),
+        template: _.template('<div id="recaptcha-<%= cid %>"></div>'),
         onDomRefresh: function () {
-            grecaptcha.render("recaptcha", {
+            if (!_.isUndefined(App.grecaptcha)) {
+                grecaptcha.reset(App.grecaptcha);
+            }
+
+            var widgetId = grecaptcha.render(this.$el[0], {
                 sitekey: this.getOption('sitekey')
             });
+
+            App.grecaptcha = widgetId;
         },
         getValue: function () {
-            return grecaptcha.getResponse();
+            return grecaptcha.getResponse(App.grecaptcha);
         },
         setValue: function () {
         },
         templateContext: function () {
             return {
                 sitekey: this.getOption('sitekey'),
-                dataField: this.getOption('dataField')
+                dataField: this.getOption('dataField'),
+                cid: this.cid
             };
         }
     });
